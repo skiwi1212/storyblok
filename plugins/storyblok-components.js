f7244f94c114f7f73caecbf9439639b680b4c30b
skiwi1212 <143973780+skiwi1212@users.noreply.github.com>
2024-11-26 15:00:10 +0000

    Fixed dependency issues by overriding globby version to 13.2.2

--- plugins/storyblok-components.js
+++ plugins/storyblok-components.js
@@ -2,6 +2,7 @@ import { defineNuxtPlugin } from '#app'
 import AllArticles from '~/components/AllArticles.vue'
 import PopularArticles from '~/components/PopularArticles.vue'
 
+
 export default defineNuxtPlugin((nuxtApp) => {
   // Register both components globally
   nuxtApp.vueApp.component('AllArticles', AllArticles)
